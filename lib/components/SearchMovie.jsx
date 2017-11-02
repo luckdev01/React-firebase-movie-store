459c5853abffa2a4ebce3ba0165dbd0becc152a2
sinstar2000 <strongweb102@gmail.com>
2017-11-02 11:16:24 +0200

    Clear search on submit

--- lib/components/SearchMovie.jsx
+++ lib/components/SearchMovie.jsx
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 
-export const SearchMovie = ({ retrieveMovieSearch }) => {
+export const SearchMovie = ({ clearQuery, userSearch, updateSearchQuery, retrieveMovieSearch }) => {
 
   let input
 
     return (
       <div className="settings-form">
-        <form
-          id='input-container'
-          onSubmit={ (e) => {
-          e.preventDefault()
-          retrieveMovieSearch(input.value)
-        }}>
-          <input
-            ref={ node => { input = node }}
-          />
-          <button
-          > Enter a Movie Title
-          </button>
-        </form>
+      <form
+      id='input-container'
+      onSubmit={ (e) => {
+        e.preventDefault()
+        retrieveMovieSearch(input.value)
+        clearQuery()
+      }}>
+      <input
+      value={userSearch}
+      onChange={(e) => updateSearchQuery(input.value)}
+      ref={ node => { input = node }}
+      />
+      <button
+      > Enter a Movie Title
+      </button>
+      </form>
       </div>
-  )
+    )
+
 }
 
 export default SearchMovie;
