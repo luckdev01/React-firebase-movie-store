a5d65a869a2895a306e18e88cd221590917b51bb
sinstar2000 <strongweb102@gmail.com>
2017-11-02 04:25:04 +0200

    Add movie poster

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -5,6 +5,7 @@ import AddMovie from './addMovie'
 import MovieCard from './MovieCard'
 import logo from '../images/reel.png'
 
+
 export default class Application extends Component {
   constructor() {
     super();
@@ -55,7 +56,7 @@ export default class Application extends Component {
         </div>
       <div>
         {this.state.movieResults.map((m, i) =>
-          <MovieCard movie={m} key={m.id} user={this.state.user}/>
+          <MovieCard movie={m}  key={m.id} user={this.state.user}/>
         )}
       </div>
       </div>
