0d75b7a6bd28e35d3fad70a83005ba5414b1de46
sinstar2000 <strongweb102@gmail.com>
2017-11-04 19:08:28 +0200

    Redirect log out/sign in

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Match, Miss } from 'react-router'
+import { signIn, signOut } from '../firebase'
+import { split } from 'lodash';
 
 import Homepage from './Homepage';
 import SearchMovie from './SearchMovie';
@@ -10,6 +12,7 @@ import SignIn from './SignIn';
 import NotFound from './NotFound';
 import Navigation from './Navigation';
 import firebase from '../firebase';
+import gravity from '../images/1.mp4'
 
 class Application extends Component{
   constructor(){
@@ -25,26 +28,41 @@ class Application extends Component{
   }
 
   render() {
+    let { user } = this.state
+    let currentUser;
+    let firstName;
+    if (user !== null) {
+      currentUser = user.displayName
+      firstName = split(user.displayName, ' ')
+    }
+
     return(
       <BrowserRouter>
-      <div>
-        <Navigation user={this.state.user}/>
-        <Match exactly pattern="/" render={() => <Homepage user={this.state.user}/>}/>
-        <Match exactly pattern="/search" render={() => <SearchMovie user={this.state.user}/>} />
-        <Match exactly pattern="/mymovies" render={() => <MyMovies user={this.state.user}/>} />
-        {/* <Match exactly pattern="/" component={MovieCard} user={this.state.user}/> */}
-        {/* <Match exactly pattern="/" component={MyMovies} /> */}
-        {/* <Match exactly pattern="/" component={App} />
-        <Match exactly pattern="/about" component={About} />
-        <Match exactly pattern="/contact" component={Contact} />*/}
-        <Miss component={NotFound} />
-      </div>
+        <div>
+          { this.state.user ?
+            <div>
+              <Navigation user={this.state.user}/>
+              <Match exactly pattern="/" render={() => <Homepage user={this.state.user}/>}/>
+              <Match exactly pattern="/search" render={() => <SearchMovie user={this.state.user}/>} />
+              <Match exactly pattern="/mymovies" render={() => <MyMovies user={this.state.user}/>} />
+              <Miss component={NotFound} />
+            </div>
+          :
+            <div>
+              <div className="video-background" >
+                <video id="my-video" className="video" autoPlay="autoplay" loop="loop" muted >
+                  <source src={gravity} type="video/mp4" />
+                  Your browser doesn't support HTML5 video. Here's a <a href="#">link</a> to the video
+                </video>
+                <div className="shadow"></div>
+              </div>
+                <button className='sign-in-btn' onClick={() => signIn()}>Sign In</button>
+            </div>
+          }
+        </div>
       </BrowserRouter>
     )
   }
 }
-
-
-
 
 render(<Application />, document.querySelector("#application"))
--- lib/components/Navigation.jsx
+++ lib/components/Navigation.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router'
-import { split } from 'lodash';
 import { signIn, signOut } from '../firebase'
-import gravity from '../images/1.mp4'
-
+import { split } from 'lodash';
 
 const Navigation = ({ user }) => {
   let currentUser;
@@ -12,10 +10,9 @@ const Navigation = ({ user }) => {
     currentUser = user.displayName
     firstName = split(user.displayName, ' ')
   }
-
-  return(
+  return (
     <div className="Navigation">
-      <div className='active-user'>{user ?
+      <div className='active-user'>
         <div>
         <ul>
           <li className="nav-link"><Link to="/">Home</Link></li>
@@ -26,17 +23,6 @@ const Navigation = ({ user }) => {
         <span className="log-in">Logged in as <span className="bold">{firstName[0]}</span><button className='auth-button button' onClick={()=> signOut()}>Sign Out</button>
         </span>
         </div>
-        :
-        <div>
-        <div className="video-background" >
-          <video id="my-video" className="video" autoPlay="autoplay" loop="loop" muted >
-            <source src={gravity} type="video/mp4" />
-          Your browser doesn't support HTML5 video. Here's a <a href="#">link</a> to the video
-          </video>
-          <div className="shadow"></div>
-        </div>
-        <button className='sign-in-btn' onClick={() => signIn()}>Sign In</button>
-        </div>}
       </div>
     </div>
   )
