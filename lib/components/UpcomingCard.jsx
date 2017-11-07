796c19ea691aa6f634ab610c3e6dd51c2644314e
sinstar2000 <strongweb102@gmail.com>
2017-11-07 09:39:24 +0200

    Genre added to upcoming

--- lib/components/UpcomingCard.jsx
+++ lib/components/UpcomingCard.jsx
@@ -45,9 +45,14 @@ export default class MovieCard extends Component {
     fetch(`https://api.themoviedb.org/3/movie/${this.state.movie.id}/videos?api_key=1500d38f789b9c7a70e564559a8c644d&language=en-US`)
     .then((response) => response.json())
     .then((response) => this.setState({ trailers: response }))
+
+    // fetch(`https://api.themoviedb.org/3/movie/${this.state.movie.id}?api_key=1500d38f789b9c7a70e564559a8c644d&language=en-US`)
+    // .then((response) => response.json())
+    // .then((data) => this.setState({ movieDetails: data }))
   }
 
   setCast() {
+
     let holy
     let trailerObj = map(this.state.trailers.results, 'key')
     if( trailerObj.length === 1 ) {
@@ -57,13 +62,41 @@ export default class MovieCard extends Component {
     }
     forEach(holy, (e) => this.setState({ youtubeID: e}))
     let cast = this.state.credits.cast
-    let genreArray = (this.state.genres.map((e) => this.genreSwitch(e)))
+    let genreArray = (this.state.movie.genre_ids.map((e) => this.genreSwitch(e)))
     this.setState({ cast: cast, genreNamesArray: genreArray })
     this.open()
   }
   open() {
      this.setState({ showModal: true });
    }
+
+  minutesConverter(minutes){
+    let hours = Math.floor(minutes / 60)
+    let newMinutes = minutes % 60
+    return `${hours} hours and ${newMinutes} minutes`
+  }
+
+   genreSwitch(genreID) {
+      if (genreID === 28){return "Action"}
+      else if (genreID === 12){return "Adventure"}
+      else if (genreID === 16){return "Animation"}
+      else if (genreID === 35){return "Comedy"}
+      else if (genreID === 80){return "Crime"}
+      else if (genreID === 99){return "Documentary"}
+      else if (genreID === 18){return "Drama"}
+      else if (genreID === 10751){return "Family"}
+      else if (genreID === 14){return "Fantasy"}
+      else if (genreID === 36){return "History"}
+      else if (genreID === 27){return "Horror"}
+      else if (genreID === 10402){return "Music"}
+      else if (genreID === 9648){return "Mystery"}
+      else if (genreID === 10749){return "Romance"}
+      else if (genreID === 878){return "Science Fiction"}
+      else if (genreID === 10770){return "TV Movie"}
+      else if (genreID === 53){return "Thriller"}
+      else if (genreID === 10752){return "War"}
+      else if (genreID === 5373){return "Western"}
+    }
 
    close() {
      this.setState({ showModal: false });
@@ -90,7 +123,8 @@ export default class MovieCard extends Component {
                     </Modal.Header>
                     <Modal.Body className="modal-body">
                       <p className="modal-director">Director: {director}</p>
-                      {/* <p className="modal-genre">Genre: {this.state.genreNamesArray.join(', ')} </p> */}
+                      <p className="modal-genre">Genre: {this.state.genreNamesArray.join(', ')} </p>
+                      {/* <p>{this.minutesConverter(`${this.state.movieDetails.runtime}`)}</p> */}
                       <p className="modal-overview">{this.state.movie.overview}</p>
                       <div className="actor-list">
                         {this.state.cast.map((m, i) =>
