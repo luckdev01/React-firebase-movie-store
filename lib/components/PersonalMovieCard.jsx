80644ac94234daf76caa528c27bf01880fd58b50
sinstar2000 <strongweb102@gmail.com>
2017-11-08 09:58:31 +0200

    Add styling to Personal modal

--- lib/components/PersonalMovieCard.jsx
+++ lib/components/PersonalMovieCard.jsx
@@ -159,21 +159,25 @@ export default class PersonalMovieCard extends Component {
                         <a name="details" />
                     <Modal.Body className="modal-body">
                     <img className="modal-backdrop" src={"https://image.tmdb.org/t/p/w500" + this.props.movie.movie.backdrop_path}  />
+                    <div className="absolute-center to-deets-abs-center">
+                    <a className="trailer-link relative-center" href="#trailer">Trailer</a>
+                    </div>
                       <div className="modal-movie-deets" >
-                      <p className="modal-crew modal-director">Director: {director}</p>
-                      <p className="modal-crew modal-genre">Genre: {this.state.genreNamesArray.join(', ')} </p>
-                      <p className="modal-runtime modal-crew" >Runtime:  {this.minutesConverter(this.state.runtime)}</p>
-                      <a href="#trailer">Trailer</a>
-                      <p className="modal-crew modal-writers">Writer(s): {writers}</p>
-                      <p className="modal-crew modal-overview">{this.props.movie.movie.overview}</p>
-                      <div  className="button-box">
-                        <form>
-                          <input className={this.state.DVD ? 'format-button format-true button' : 'format-button format-false button'} type="button" value="DVD" onClick={() => this.updateFormat('DVD')}/>
-                          <input className={this.state.Bluray ? 'format-button format-true button' : 'format-button format-false button'} type="button" value="Blu-ray" onClick={() => this.updateFormat('Bluray')}/>
-                          <input className={this.state.iTunes ? 'format-button format-true button' : 'format-button format-false button'} type="button" value="iTunes" onClick={() => this.updateFormat('iTunes')}/>
-                          <input className={this.state.Prime ? 'format-button format-true button' : 'format-button format-false button'} type="button" value="Prime" onClick={() => this.updateFormat('Prime')}/>
-                        </form>
+                      <p className="modal-crew">
+                        Director: {director} <br/><br/>
+                        Genre: {this.state.genreNamesArray.join(', ')} <br/><br/>
+                        Runtime:  {this.minutesConverter(this.state.runtime)} <br/><br/>
+                        Writer(s): {writers} <br/><br/>
+                        Plot: {this.props.movie.movie.overview}
+                      </p>
                       </div>
+                      <div  className="modal-button-box">
+                      <form>
+                      <input className={this.state.DVD ? 'modal-format-button format-true button' : 'modal-format-button format-false button'} type="button" value="DVD" onClick={() => this.updateFormat('DVD')}/>
+                      <input className={this.state.Bluray ? 'modal-format-button format-true button' : 'modal-format-button format-false button'} type="button" value="Blu-ray" onClick={() => this.updateFormat('Bluray')}/>
+                      <input className={this.state.iTunes ? 'modal-format-button format-true button' : 'modal-format-button format-false button'} type="button" value="iTunes" onClick={() => this.updateFormat('iTunes')}/>
+                      <input className={this.state.Prime ? 'modal-format-button format-true button' : 'modal-format-button format-false button'} type="button" value="Prime" onClick={() => this.updateFormat('Prime')}/>
+                      </form>
                       </div>
                       <div className="actor-list">
                         {this.state.cast.map((m, i) =>
@@ -181,7 +185,9 @@ export default class PersonalMovieCard extends Component {
                         )}
                       </div>
                       <div className="youtube-container">
-                      <a name="trailer" href="#details">Back to Details</a>
+                      <div className="absolute-center back-to-deets-abs-center">
+                        <a className="relative-center" name="trailer" href="#details">Back to Details</a>
+                      </div>
                       { this.state.youtubeID ?
                         <YouTube
                           className="youtube"
