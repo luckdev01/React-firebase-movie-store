93439b02fbddd675f8b6b50e895e25659a706836
sinstar2000 <strongweb102@gmail.com>
2017-11-11 06:54:48 +0200

    Remove recreated images

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -50,7 +50,7 @@ class Application extends Component{
             <div>
               <div className="video-background" >
                 <video id="my-video" className="video" autoPlay="autoplay" loop="loop" muted >
-                  <source src="https://11-lvl3-pdl.vimeocdn.com/01/178/4/100893658/271010377.mp4?expires=1484262948&token=0a651f963359e58da5521" type="video/mp4" />
+                  <source src={gravity} type="video/mp4" />
                   Your browser doesn't support HTML5 video. Here's a <a href="#">link</a> to the video
                 </video>
                 <div className="shadow"></div>
