004ad93b4dc124bdc22c78ab65c8f1e989a5ed7c
sinstar2000 <strongweb102@gmail.com>
2017-11-12 19:33:40 +0200

    Set up tests

---
+++ test/application.spec.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { shallow, mount, render } from 'enzyme';
+import { assert, expect } from 'chai';
+let sinon = require('sinon');
+import moment from 'moment';
+import locus from 'locus';
+
+import Application from '../lib/components/Application';
+
+describe('Application', () => {
+
+  it('renders as a <div>', () => {
+    const wrapper = shallow(<Application />);
+    assert.equal(wrapper.type(), 'div');
+  });
+
+  // it('calls componentDidMount', () => {
+  //   sinon.spy(Application.prototype, 'componentDidMount');
+  //       const wrapper = mount(<Application />);
+  //       expect(Application.prototype.componentDidMount.calledOnce).to.equal(true);
+  // });
+
+});
