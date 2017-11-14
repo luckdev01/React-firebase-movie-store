71929e4e86aa3584a54feee3d1a9a76f31ad09b3
sinstar2000 <strongweb102@gmail.com>
2017-11-14 22:50:52 +0200

    lint upcoming

--- lib/components/Upcoming.jsx
+++ lib/components/Upcoming.jsx
@@ -1,43 +1,38 @@
 import React, { Component } from 'react';
-import npa from '../images/no-poster.png'
-import firebase from '../firebase';
-import UpcomingCard from './UpcomingCard'
-import PopularMovieCard from './PopularMovieCard'
+import UpcomingCard from './UpcomingCard';
+import PopularMovieCard from './PopularMovieCard';
 
 export default class Upcoming extends Component {
   constructor() {
     super();
     this.state = {
       user: null,
-      movieResults: [],
-      popular: []
-    }
+      upcoming: [],
+      popular: [],
+    };
   }
 
   componentWillMount() {
-    let user = this.props.user
-    this.setState({ user })
+    const user = this.props.user;
+    this.setState({ user });
 
     fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=1500d38f789b9c7a70e564559a8c644d&language=en-US&page=1`)
-    .then((response) => response.json())
-    .then((data) => data)
-    .then((movies) => this.setState({ movieResults: movies.results}))
+    .then(response => response.json())
+    .then(movies => this.setState({ upcoming: movies.results }));
 
     fetch(`https://api.themoviedb.org/3/movie/popular?api_key=1500d38f789b9c7a70e564559a8c644d&language=en-US&page=1`)
-    .then((response) => response.json())
-    .then((data) => data)
-    .then((movies) => this.setState({ popular: movies.results}))
+    .then(response => response.json())
+    .then(movies => this.setState({ popular: movies.results }));
   }
 
 
   render() {
-
     return (
       <div>
       <h3 className="upcoming-header">Recent & Upcoming</h3>
       <article className="upcoming-card-container">
         <div className="upcoming-card-container-inner">
-        {this.state.movieResults.map((m, i) =>
+        {this.state.upcoming.map(m =>
           <UpcomingCard movie={m} key={m.id} user={this.state.user}/>
         )}
         </div>
@@ -45,12 +40,12 @@ export default class Upcoming extends Component {
       <h3 className="upcoming-header">Popular Movies</h3>
       <article className="upcoming-card-container">
         <div className="upcoming-card-container-inner">
-        {this.state.popular.map((m, i) =>
+        {this.state.popular.map(m =>
           <PopularMovieCard movie={m} key={m.id} user={this.state.user}/>
         )}
         </div>
       </article>
       </div>
-    )
+    );
   }
 }
