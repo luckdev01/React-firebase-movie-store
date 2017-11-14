9a84ab37dff16b3d72ed2627248185bd104ecf9b
sinstar2000 <strongweb102@gmail.com>
2017-11-14 22:18:08 +0200

    Lint navigation

--- lib/components/Navigation.jsx
+++ lib/components/Navigation.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router'
-import { signIn, signOut } from '../firebase'
+import { signOut } from '../firebase'
 import { split } from 'lodash';
 
 const Navigation = ({ user }) => {
   let currentUser;
   let firstName;
   if (user !== null) {
-    currentUser = user.displayName
-    firstName = split(user.displayName, ' ')
+    currentUser = user.displayName;
+    firstName = split(user.displayName, ' ');
   }
   return (
     <div className="navigation">
@@ -18,13 +18,17 @@ const Navigation = ({ user }) => {
         <li className="nav-link"><Link to="/">My Movies</Link></li>
           <li className="nav-link"><Link to="/explore">Explore</Link></li>
           <li className="nav-link"><Link to="/search">Search</Link></li>
-          {/* <li className="nav-link"><Link to="/about">About</Link></li> */}
         </ul>
-        <span className="log-in"><button className='auth-button sign-out-button button' onClick={()=> signOut()}>Sign Out</button>
+        <span className="log-in">
+          <button
+            className='auth-button sign-out-button button'
+            onClick={() => signOut()}>
+            Sign Out
+          </button>
         </span>
         </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
