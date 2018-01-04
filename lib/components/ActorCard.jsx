fabd01bc52ebf208e6a0999117fb2645d932c9e8
sinstar2000 <strongweb102@gmail.com>
2018-01-04 23:11:42 +0200

    Update actor card to dumb component

--- lib/components/ActorCard.jsx
+++ lib/components/ActorCard.jsx
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import npa from '../images/no-image.png'
 
-export default class ActorCard extends Component {
-  render() {
+export const ActorCard = ({ cast }) => {
     return (
       <article className="actor-card">
-          { this.props.cast.profile_path ?
-            <img className="actor-image" src={'https://image.tmdb.org/t/p/w500' + this.props.cast.profile_path}/>
+          { cast.profile_path ?
+            <img className="actor-image" src={'https://image.tmdb.org/t/p/w500' + cast.profile_path}/>
           :
             <img src={npa} className="actor-image"/>
           }
-          <p className="actor-card-character">{this.props.cast.character}</p>
-          <p className="actor-card-name">{this.props.cast.name}</p>
+          <p className="actor-card-character">{cast.character}</p>
+          <p className="actor-card-name">{cast.name}</p>
       </article>
     );
-  }
 }
