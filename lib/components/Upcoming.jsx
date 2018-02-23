3cdfd49958f54aadbcf60b6a8613465b76f3acce
sinstar2000 <strongweb102@gmail.com>
2018-02-23 04:45:11 +0200

    Click explore to fetch data

--- lib/components/Upcoming.jsx
+++ lib/components/Upcoming.jsx
@@ -10,27 +10,42 @@ export default class Upcoming extends Component {
       user: null,
       upcoming: [],
       popular: [],
+      recentShow: false,
+      popularShow: false
     };
   }
 
-  componentWillMount() {
+  componentDidMount() {
     const user = this.props.user;
     this.setState({ user });
+  }
 
-    fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=1500d38f789b9c7a70e564559a8c644d&language=en-US&page=1`)
-    .then(response => response.json())
-    .then(movies => this.setState({ upcoming: movies.results }));
+  fetchRecent() {
+    this.setState({ recentShow: !this.state.recentShow})
 
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=1500d38f789b9c7a70e564559a8c644d&language=en-US&page=1`)
-    .then(response => response.json())
-    .then(movies => this.setState({ popular: movies.results }));
+    if(this.state.upcoming == 0){
+      fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=1500d38f789b9c7a70e564559a8c644d&language=en-US&page=1`)
+      .then(response => response.json())
+      .then(movies => this.setState({ upcoming: movies.results }));
+    } else { null }  
+  }
+
+  fetchPopular(){
+    this.setState({ popularShow: !this.state.popularShow})
+
+    if(this.state.popular.length == 0) {
+      fetch(`https://api.themoviedb.org/3/movie/popular?api_key=1500d38f789b9c7a70e564559a8c644d&language=en-US&page=1`)
+      .then(response => response.json())
+      .then(movies => this.setState({ popular: movies.results }))
+    } else { null }
   }
 
 
   render() {
     return (
       <div className="explore">
-      <h3 className="upcoming-header">Recent & Upcoming</h3>
+      <h3 className="upcoming-header" onClick={() => this.fetchRecent()}>Recent & Upcoming</h3>
+        { this.state.recentShow ?
         <article className="upcoming-card-container">
           <div className="upcoming-card-container-inner">
           {this.state.upcoming.map(m =>
@@ -38,14 +53,21 @@ export default class Upcoming extends Component {
           )}
           </div>
         </article>
-      {/* <h3 className="upcoming-header">Popular Movies</h3>
+        :
+        null
+      }
+      <h3 className="upcoming-header" onClick={() => this.fetchPopular()}>Popular Movies</h3>
+        {this.state.popularShow ?
         <article className="upcoming-card-container">
           <div className="upcoming-card-container-inner">
           {this.state.popular.map(m =>
             <PopularMovieCard movie={m} key={m.id} user={this.state.user}/>
           )}
           </div>
-        </article> */}
+        </article>
+        :
+        null
+      }
       </div>
     );
   }
